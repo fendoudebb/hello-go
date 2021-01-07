6f9f471b1a208a84741b42cff8071c0ff3583b6e
fendoudebb <15317832520@163.com>
2021-01-07 09:50:33 +0800

    http file server

--- net/http/api_http_file_server.go
+++ net/http/api_http_file_server.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
+func main() {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal("get wd error#", err.Error())
+	}
+	http.Handle("/net/", http.StripPrefix("/net/", http.FileServer(http.Dir(wd + "/net"))))
+
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(wd + "/builtin"))))
+
+	if err := http.ListenAndServe(":9999", nil); err != nil {
+		log.Fatal("Listen Fail#", err.Error())
+	}
 }
