2e2de1accd82ee6326f3f954e4aa9a89d72e8c82
fendoudebb <15317832520@163.com>
2021-01-21 20:30:11 +0800

    comment

---
+++ comment/usage_comment.go
@@ -0,0 +1,19 @@
+package comment
+
+// Add return a and b's sum
+func Add(a, b int) int {
+	return a + b
+}
+
+// User contains age and name
+type User struct {
+	age  int
+	name string
+}
+
+/*
+多行注释
+ */
+func unfinishedFunc() {
+	// TODO unfinished func
+}
