677faed5f53a5e333e720c81ddca057f16392f0b
fendoudebb <15317832520@163.com>
2021-02-05 10:31:31 +0800

    time

--- strings/api_strconv.go
+++ strings/api_strconv.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"time"
 )
 
 func main() {
@@ -16,6 +17,12 @@ func main() {
 	// 32位float，64位float
 	float, err := strconv.ParseFloat("2020.1025", 64)
 	fmt.Println(float, err)
+
+	var age = 18
+	fmt.Println(strconv.Itoa(age))
+
+	unixTimestamp := time.Now().Unix()
+	fmt.Println(strconv.FormatInt(unixTimestamp, 10))
 
 
 }
